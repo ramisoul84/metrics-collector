65b441cf83d9108ec8e9fb1862397eaf4454685e
Rami <rami.suliman@mail.ru>
2025-02-08 19:45:26 +0300

    get env

--- cmd/collector/config.go
+++ cmd/collector/config.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/caarlos0/env/v6"
+)
+
+type Config struct {
+	LogLevel          string `env:"LOG_LEVEL" envDefault:"DEBUG"`
+	EnableDebugServer bool   `env:"ENABLE_DEBUG_SERVER" envDefault:"true"`
+	DebugServerAddr   string `env:"DEBUG_SERVER_ADDR" envDefault:"0.0.0.0:8181"`
+}
+
+func ReadConfig() error {
+	config := Config{}
+	env.Parse(&config)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/ramisoul84/metrics-collector
 
 go 1.23.4
 
-require go.uber.org/zap v1.27.0
-
 require (
-	github.com/caarlos0/env/v6 v6.10.1 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
+	github.com/caarlos0/env/v6 v6.10.1
+	go.uber.org/zap v1.27.0
 )
+
+require go.uber.org/multierr v1.10.0 // indirect
